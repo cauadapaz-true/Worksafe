16274e3d7647497b63bf5fd2bc4447e7a9b7ce5f
Cauã da Paz <cauadapaz2023@gmail.com>
2026-05-20 21:20:06 -0300

    criação do package e classe connection

--- src/Connection/Conexao.java
+++ src/Connection/Conexao.java
@@ -7,43 +7,29 @@ import java.sql.SQLException;
 public class Conexao {
 
     private static Conexao instance;
-
     public final Connection conexao;
-
     private final String url =
             "jdbc:postgresql://db.bvppxnzbndjmbysrygxt.supabase.co:5432/postgres?sslmode=require";
-
     private final String user = "postgres";
-
-    private final String password =
-            "projetobancodedados";
+    private final String password = "projetobancodedados";
 
     private Conexao() {
 
         try {
 
             this.conexao =
-                    DriverManager.getConnection(
-                            url,
-                            user,
-                            password
-                    );
+                    DriverManager.getConnection(url, user, password);
 
         } catch (SQLException ex) {
 
-            throw new RuntimeException(
-                    "Erro ao conectar: "
-                            + ex.getMessage()
-            );
+            throw new RuntimeException("Erro ao conectar: "+ ex.getMessage());
         }
     }
 
     public static Conexao getInstance() {
-
         if (instance == null) {
             instance = new Conexao();
         }
-
         return instance;
     }
 
